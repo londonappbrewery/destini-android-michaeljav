ea43e61f99a80b62455dcc5cbce6d282db8b262f
mjavier <michaeljaviermota@gmail.com>
2018-04-20 11:50:56 -0400

    Trabajando

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,6 +2,7 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
    private TextView  storyTextView ;
    private Button  buttonTop;
    private  Button  buttonBottom;
+   private  int trackStory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +24,41 @@ public class MainActivity extends AppCompatActivity {
         storyTextView = findViewById(R.id.storyTextView);
         buttonTop = findViewById(R.id.buttonTop);
         buttonBottom = findViewById(R.id.buttonBottom);
-
+        trackStory = 1;
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+         buttonTop.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
 
+                 if (trackStory == 1) {
+                     updateViewTextButton(R.string.T3_Story,R.string.T3_Ans1,R.string.T3_Ans2
+                     ,3);
+                 }
 
+             }
+         });
+         buttonBottom.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+
+             }
+         });
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
 
 
     }
+    private void updateViewTextButton(int text, int btnTop, int btnBottom,int straStory){
+
+        storyTextView.setText(text);
+        buttonTop.setText(btnTop);
+        buttonBottom.setText(btnBottom);
+        trackStory = straStory;
+
+    }
+
+
+
 }
